bb24ad36160a834c7c6f973df2235e926cdbebc7
pzmarzly <pawo2500@gmail.com>
2020-10-05 09:53:03 +0200

    6 columns, add package-lock.json

--- src/script.js
+++ src/script.js
@@ -1,36 +1,36 @@
-moment.locale('pl');
+moment.locale("pl");
 
-let data0 = moment(prompt('Start od [DD-MM-RRRR]: '), 'DD-MM-YYYY').unix();
-let data1 = moment(prompt('Do [DD-MM-RRRR]: '), 'DD-MM-YYYY').unix();
+let data0 = moment(prompt("Start date [DD-MM-RRRR]: "), "DD-MM-YYYY").unix();
+let data1 = moment(prompt("End date [DD-MM-RRRR]: "), "DD-MM-YYYY").unix();
 
-let godz0 = parseInt(prompt('Godzina startu [HH]: '), 10);
-let godz1 = parseInt(prompt('Do [HH]: '), 10);
+let godz0 = parseInt(prompt("First hour [HH]: "), 10);
+let godz1 = parseInt(prompt("Last hour [HH]: "), 10);
 
 new Vue({
-    el: '#main',
-    data() {
-        return {
-            date0: data0,
-            date1: data1,
-        }
+  el: "#main",
+  data() {
+    return {
+      date0: data0,
+      date1: data1,
+    };
+  },
+  methods: {
+    wysw(i) {
+      let m = moment.unix(data0);
+      m.add(i - 1, "d");
+      let ret = "";
+      if (m.day() == 0) ret += "<span class='red'>";
+      ret += m.format("dddd, Do MMM YYYY");
+      if (m.day() == 0) ret += "</span>";
+      return ret;
     },
-    methods: {
-        wysw(i) {
-            let m = moment.unix(data0);
-            m.add((i - 1), 'd');
-            let ret = "";
-            if (m.day() == 0) ret += "<span class='red'>";
-            ret += m.format("dddd, Do MMM YYYY");
-            if (m.day() == 0) ret += "</span>";
-            return ret;
-        },
-        godz(i) {
-            let m = moment('2000 1', 'Y DDD');
-            m.add(godz0, 'h');
-            m.add(30 * (i - 1), 'm');
-            return m.format('HH:mm');
-        },
+    godz(i) {
+      let m = moment("2000 1", "Y DDD");
+      m.add(godz0, "h");
+      m.add(30 * (i - 1), "m");
+      return m.format("HH:mm");
     },
+  },
 });
 
-document.body.classList.add('loaded');
+document.body.classList.add("loaded");
